77d952de3695ee58d4fda54b0a2bab04378922c3
Hauke Heibel <haukeheibel@google.com>
2024-07-15 03:22:13 -0700

    SDK update
    GitOrigin-RevId: f75c2f5d70e0fed201dbd97d2f6f1ea171332ccb

--- intrinsic/util/thread/stop_token.h
+++ intrinsic/util/thread/stop_token.h
@@ -37,10 +37,12 @@ using SharedStopState = std::shared_ptr<StopState>;
 
 }  // namespace detail
 
+class StopSource;
+
 // Token to be passed to threads to communicate cancellations, for example in
 // context of deadlines or when the client cancels the request. Follows
-// std::stop_token terminology; The intent is to replace this class eventually
-// by std::stop_token. This class is thread safe.
+// std::stop_token semantics and API; The intent is to replace this class
+// eventually by std::stop_token. This class is thread safe.
 class StopToken {
  public:
   // Constructs an empty StopToken with no associated stop-state.
@@ -49,10 +51,6 @@ class StopToken {
   // Default copy and move constructors.
   StopToken(const StopToken& other) noexcept = default;
   StopToken(StopToken&& other) noexcept = default;
-
-  // Constructs a StopToken with the given stop-state.
-  explicit StopToken(detail::SharedStopState state)
-      : stop_state_(std::move(state)) {}
 
   ABSL_MUST_USE_RESULT bool stop_requested() const noexcept {
     return stop_state_ != nullptr && stop_state_->stopped;
@@ -63,28 +61,50 @@ class StopToken {
   }
 
  private:
+  // Required to make the constructor below private.
+  friend class StopSource;
+
+  // Constructs a StopToken with the given stop-state.
+  explicit StopToken(detail::SharedStopState state)
+      : stop_state_(std::move(state)) {}
+
   detail::SharedStopState stop_state_ = nullptr;
 };
 
+// Source of StopTokens and the interface to request a stop.
 class StopSource {
  public:
+  // Constructs a StopSource with new stop-state.
   explicit StopSource() : stop_state_(std::make_shared<detail::StopState>()) {}
+
+  // Constructs an empty StopSource with no associated stop-state.
   explicit StopSource(detail::NoStopStateType /*unused*/) noexcept {}
 
+  // Default copy and move constructors.
   StopSource(const StopSource& other) noexcept = default;
   StopSource(StopSource&& other) noexcept = default;
 
+  // Issues a stop request to the stop-state, if the stop_source object has a
+  // stop-state and it has not yet already had stop requested.
   bool request_stop() const noexcept {
     return stop_possible() && stop_state_->request_stop();
   }
 
+  // Returns a StopToken object, which will be empty if stop_possible() ==
+  // false.
   StopToken get_token() const noexcept { return StopToken(stop_state_); }
 
-  bool stop_requested() const noexcept {
+  // Checks whether the associated stop-state has been requested to stop.
+  ABSL_MUST_USE_RESULT bool stop_requested() const noexcept {
     return stop_state_ != nullptr && stop_state_->stopped;
   }
 
-  bool stop_possible() const noexcept { return stop_state_ != nullptr; }
+  // Checks whether associated stop-state can be requested to stop.
+  // If the StopSource object has a stop-state and a stop request has already
+  // been made, this function still returns true.
+  ABSL_MUST_USE_RESULT bool stop_possible() const noexcept {
+    return stop_state_ != nullptr;
+  }
 
  private:
   detail::SharedStopState stop_state_ = nullptr;
