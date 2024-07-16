84b63226e2a0c95dd748aaae0a6183a703682e3e
Hauke Heibel <haukeheibel@google.com>
2024-07-15 23:48:19 -0700

    SDK update
    GitOrigin-RevId: 338ad5d8dc0b7a72bd1b4def149c0dcdf689a761

--- intrinsic/util/thread/stop_token_test.cc
+++ intrinsic/util/thread/stop_token_test.cc
@@ -7,6 +7,34 @@
 namespace intrinsic {
 namespace {
 
+// StopSource tests
+
+TEST(StopSourceTest, StopSourceWithNoState) {
+  StopSource stop_source(detail::NoState);
+  EXPECT_FALSE(stop_source.stop_possible());
+  EXPECT_FALSE(stop_source.stop_requested());
+}
+
+TEST(StopSourceTest, StopSourceWithState) {
+  StopSource stop_source;
+  EXPECT_TRUE(stop_source.stop_possible());
+  EXPECT_FALSE(stop_source.stop_requested());
+}
+
+TEST(StopSourceTest, StopSourceCanBeStopped) {
+  StopSource stop_source;
+  EXPECT_TRUE(stop_source.request_stop());
+  EXPECT_TRUE(stop_source.stop_requested());
+}
+
+TEST(StopSourceTest, StopSourceWithoutStateCannotBeStopped) {
+  StopSource stop_source(detail::NoState);
+  EXPECT_FALSE(stop_source.stop_possible());
+  EXPECT_FALSE(stop_source.request_stop());
+}
+
+// StopToken tests
+
 TEST(StopTokenTest, StopSourceWithNoState) {
   StopToken stop_token;
   EXPECT_FALSE(stop_token.stop_possible());
