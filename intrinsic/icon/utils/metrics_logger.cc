ec973f5e298959df326886c8dc73b5312361ab0e
Copybara-Service <copybara-worker@google.com>
2024-07-18 08:04:12 -0700

    SDK update
    GitOrigin-RevId: fb9fbd415d9ceca43809849445eac5b4dc306394

---
+++ intrinsic/icon/utils/metrics_logger.cc
@@ -0,0 +1,74 @@
+// Copyright 2023 Intrinsic Innovation LLC
+
+#include "intrinsic/icon/utils/metrics_logger.h"
+
+#include <cstddef>
+#include <string>
+#include <utility>
+
+#include "absl/status/status.h"
+#include "google/protobuf/struct.pb.h"
+#include "intrinsic/logging/data_logger_client.h"
+#include "intrinsic/logging/proto/log_item.pb.h"
+#include "intrinsic/performance/analysis/proto/performance_metrics.pb.h"
+#include "intrinsic/platform/common/buffers/realtime_write_queue.h"
+#include "intrinsic/util/status/status_macros.h"
+#include "intrinsic/util/thread/thread.h"
+
+using intrinsic_proto::data_logger::LogItem;
+using intrinsic_proto::performance::analysis::proto::PerformanceMetrics;
+
+namespace intrinsic::icon {
+
+static constexpr std::size_t kRtQueueSize = 1000;
+
+MetricsLogger::~MetricsLogger() {
+  if (metrics_publisher_thread_.Joinable()) {
+    shutdown_requested_.store(true);
+    metrics_publisher_thread_.Join();
+  }
+}
+
+absl::Status MetricsLogger::Start() {
+  if (metrics_publisher_thread_.Joinable()) {
+    return absl::FailedPreconditionError(
+        "Metrics publisher thread is already running");
+  }
+
+  intrinsic::Thread::Options options;
+  options.SetNormalPriorityAndScheduler();
+  options.SetName("metrics_publisher_thread_");
+  shutdown_requested_.store(false);
+  INTR_RETURN_IF_ERROR(
+      metrics_publisher_thread_.Start(options, [this]() { LoggerFunction(); }));
+
+  return absl::OkStatus();
+}
+void MetricsLogger::LoggerFunction() {
+  // Read the queue until empty
+  while (!shutdown_requested_.load()) {
+    // Build performance metrics log
+    LogItem log_item = BuildMetricsLog();
+  }
+}
+
+// Helper functions to build performance metrics proto
+void InsertNumericField(PerformanceMetrics& perf_metrics,
+                        std::string field_name, double field_value) {
+  google::protobuf::Value field_value_proto;
+  field_value_proto.set_number_value(field_value);
+  perf_metrics.mutable_metrics()->mutable_metrics()->mutable_fields()->insert(
+      std::make_pair(field_name, field_value_proto));
+}
+
+void InsertStringField(PerformanceMetrics& perf_metrics, std::string field_name,
+                       std::string field_value) {
+  google::protobuf::Value field_value_proto;
+  field_value_proto.set_string_value(field_value);
+  perf_metrics.mutable_metrics()->mutable_metrics()->mutable_fields()->insert(
+      std::make_pair(field_name, field_value_proto));
+}
+
+LogItem MetricsLogger::BuildMetricsLog() {}
+
+}  // namespace intrinsic::icon
---
+++ intrinsic/icon/utils/metrics_logger.h
@@ -0,0 +1,47 @@
+// Copyright 2023 Intrinsic Innovation LLC
+
+#ifndef INTRINSIC_ICON_UTILS_METRICS_LOGGER_H_
+#define INTRINSIC_ICON_UTILS_METRICS_LOGGER_H_
+
+#include <stdbool.h>
+
+#include <atomic>
+#include <string>
+
+#include "absl/status/status.h"
+#include "intrinsic/icon/testing/realtime_annotations.h"
+#include "intrinsic/logging/proto/log_item.pb.h"
+#include "intrinsic/performance/analysis/proto/performance_metrics.pb.h"
+#include "intrinsic/platform/common/buffers/realtime_write_queue.h"
+#include "intrinsic/util/thread/thread.h"
+
+namespace intrinsic::icon {
+
+using ::intrinsic_proto::data_logger::LogItem;
+using ::intrinsic_proto::performance::analysis::proto::PerformanceMetrics;
+
+// A non-realtime logger that can be used to log messages from realtime
+// contexts.
+class MetricsLogger {
+ public:
+  explicit MetricsLogger(std::string module_name);
+
+  ~MetricsLogger();
+
+  // Starts the metrics logger thread
+  absl::Status Start();
+
+ private:
+  // Thread function
+  void LoggerFunction();
+  LogItem BuildMetricsLog();
+  //  Thread to publish metrics (non-real-time)
+  intrinsic::Thread metrics_publisher_thread_;
+  // Atomic flag to enable/disable the metrics thread.
+  std::atomic<bool> shutdown_requested_;
+  // The name of the module that is logging metrics
+  std::string module_name_;
+};
+}  // namespace intrinsic::icon
+
+#endif  // INTRINSIC_ICON_UTILS_METRICS_LOGGER_H_
