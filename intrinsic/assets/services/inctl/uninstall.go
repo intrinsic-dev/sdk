cb2c5ff244fa6c3eb1422ae90f1e5da588bc7fb7
Copybara-Service <copybara-worker@google.com>
2024-08-30 09:50:40 -0700

    SDK update
    GitOrigin-RevId: 5005ea2820a7d7baf84be0997a2ef0ee61f3f6ba

--- intrinsic/assets/services/inctl/uninstall.go
+++ intrinsic/assets/services/inctl/uninstall.go
@@ -11,17 +11,16 @@ import (
 	"intrinsic/assets/clientutils"
 	"intrinsic/assets/cmdutils"
 	"intrinsic/assets/idutils"
-	"intrinsic/assets/version"
-	installergrpcpb "intrinsic/kubernetes/workcell_spec/proto/installer_go_grpc_proto"
-	installerpb "intrinsic/kubernetes/workcell_spec/proto/installer_go_grpc_proto"
-	rrgrpcpb "intrinsic/resources/proto/resource_registry_go_grpc_proto"
+	idpb "intrinsic/assets/proto/id_go_proto"
+	iagrpcpb "intrinsic/assets/proto/installed_assets_go_grpc_proto"
+	iapb "intrinsic/assets/proto/installed_assets_go_grpc_proto"
 )
 
 // GetCommand returns a command to uninstall a service.
 func GetCommand() *cobra.Command {
 	flags := cmdutils.NewCmdFlags()
 	cmd := &cobra.Command{
-		Use:   "uninstall ID|ID_VERSION",
+		Use:   "uninstall ID",
 		Short: "Remove a Service type (Note: This will fail if there are instances of it in the solution)",
 		Example: `
 		$ inctl service uninstall ai.intrinsic.realtime_control_service \
@@ -37,10 +36,13 @@ func GetCommand() *cobra.Command {
 		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			ctx := cmd.Context()
-			idOrIDVersion := args[0]
-			idv, err := idutils.IDOrIDVersionProtoFrom(idOrIDVersion)
+			id := args[0]
+			idv, err := idutils.IDOrIDVersionProtoFrom(id)
 			if err != nil {
 				return fmt.Errorf("invalid identifier: %v", err)
+			}
+			if v := idv.GetVersion(); v != "" {
+				log.Print("Warning: specifying the version of an asset is deprecated, and soon will cause an error")
 			}
 
 			ctx, conn, _, err := clientutils.DialClusterFromInctl(ctx, flags)
@@ -49,24 +51,15 @@ func GetCommand() *cobra.Command {
 			}
 			defer conn.Close()
 
-			if err := version.Autofill(ctx, rrgrpcpb.NewResourceRegistryClient(conn), idv); err != nil {
-				return err
-			}
-
-			client := installergrpcpb.NewInstallerServiceClient(conn)
-			_, err = client.UninstallService(ctx, &installerpb.UninstallServiceRequest{
-				IdVersion: idv,
-			})
-			if err != nil {
+			client := iagrpcpb.NewInstalledAssetsClient(conn)
+			if _, err := client.DeleteInstalledAssets(ctx, &iapb.DeleteInstalledAssetsRequest{
+				Assets: []*idpb.Id{
+					idv.GetId(),
+				},
+			}); err != nil {
 				return fmt.Errorf("could not uninstall the service: %w", err)
 			}
-			// Ignore the errors, since it was somehow successful already, just
-			// use this to provide more clarity about exactly which version was
-			// removed.
-			if idvStr, err := idutils.IDVersionFromProto(idv); err != nil {
-				idOrIDVersion = idvStr
-			}
-			log.Printf("Finished uninstalling %q", idOrIDVersion)
+			log.Printf("Finished uninstalling %q", id)
 
 			return nil
 		},
