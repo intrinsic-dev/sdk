e8e0ba67f5ae1d70a435ee1a9cba8f56a16291da
Copybara-Service <copybara-worker@google.com>
2024-09-04 16:33:16 -0700

    SDK update
    GitOrigin-RevId: 8bedf347e2e80f96616c9de6b7adb68abe4f3a65

--- intrinsic/assets/services/inctl/uninstall.go
+++ intrinsic/assets/services/inctl/uninstall.go
@@ -6,22 +6,25 @@ package uninstall
 import (
 	"fmt"
 	"log"
+	"time"
 
+	lrogrpcpb "cloud.google.com/go/longrunning/autogen/longrunningpb"
+	lropb "cloud.google.com/go/longrunning/autogen/longrunningpb"
 	"github.com/spf13/cobra"
+	"google.golang.org/grpc/status"
 	"intrinsic/assets/clientutils"
 	"intrinsic/assets/cmdutils"
 	"intrinsic/assets/idutils"
-	"intrinsic/assets/version"
-	installergrpcpb "intrinsic/kubernetes/workcell_spec/proto/installer_go_grpc_proto"
-	installerpb "intrinsic/kubernetes/workcell_spec/proto/installer_go_grpc_proto"
-	rrgrpcpb "intrinsic/resources/proto/resource_registry_go_grpc_proto"
+	idpb "intrinsic/assets/proto/id_go_proto"
+	iagrpcpb "intrinsic/assets/proto/installed_assets_go_grpc_proto"
+	iapb "intrinsic/assets/proto/installed_assets_go_grpc_proto"
 )
 
 // GetCommand returns a command to uninstall a service.
 func GetCommand() *cobra.Command {
 	flags := cmdutils.NewCmdFlags()
 	cmd := &cobra.Command{
-		Use:   "uninstall ID|ID_VERSION",
+		Use:   "uninstall ID",
 		Short: "Remove a Service type (Note: This will fail if there are instances of it in the solution)",
 		Example: `
 		$ inctl service uninstall ai.intrinsic.realtime_control_service \
@@ -37,10 +40,13 @@ func GetCommand() *cobra.Command {
 		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			ctx := cmd.Context()
-			idOrIDVersion := args[0]
-			idv, err := idutils.IDOrIDVersionProtoFrom(idOrIDVersion)
+			id := args[0]
+			idv, err := idutils.IDOrIDVersionProtoFrom(id)
 			if err != nil {
 				return fmt.Errorf("invalid identifier: %v", err)
+			}
+			if v := idv.GetVersion(); v != "" {
+				log.Print("Warning: specifying the version of an asset is deprecated, and soon will cause an error")
 			}
 
 			ctx, conn, _, err := clientutils.DialClusterFromInctl(ctx, flags)
@@ -49,24 +55,32 @@ func GetCommand() *cobra.Command {
 			}
 			defer conn.Close()
 
-			if err := version.Autofill(ctx, rrgrpcpb.NewResourceRegistryClient(conn), idv); err != nil {
-				return err
-			}
-
-			client := installergrpcpb.NewInstallerServiceClient(conn)
-			_, err = client.UninstallService(ctx, &installerpb.UninstallServiceRequest{
-				IdVersion: idv,
+			client := iagrpcpb.NewInstalledAssetsClient(conn)
+			op, err := client.DeleteInstalledAssets(ctx, &iapb.DeleteInstalledAssetsRequest{
+				Assets: []*idpb.Id{
+					idv.GetId(),
+				},
 			})
 			if err != nil {
 				return fmt.Errorf("could not uninstall the service: %w", err)
 			}
-			// Ignore the errors, since it was somehow successful already, just
-			// use this to provide more clarity about exactly which version was
-			// removed.
-			if idvStr, err := idutils.IDVersionFromProto(idv); err != nil {
-				idOrIDVersion = idvStr
+
+			log.Printf("Awaiting completion of the removal")
+			lroClient := lrogrpcpb.NewOperationsClient(conn)
+			for !op.GetDone() {
+				time.Sleep(15 * time.Millisecond)
+				op, err = lroClient.GetOperation(ctx, &lropb.GetOperationRequest{
+					Name: op.GetName(),
+				})
+				if err != nil {
+					return fmt.Errorf("unable to check status of removal: %v", err)
+				}
 			}
-			log.Printf("Finished uninstalling %q", idOrIDVersion)
+
+			if err := status.ErrorProto(op.GetError()); err != nil {
+				return fmt.Errorf("removal failed: %w", err)
+			}
+			log.Printf("Finished uninstalling %q", id)
 
 			return nil
 		},
