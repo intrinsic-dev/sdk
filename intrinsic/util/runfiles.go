c6091931fdf9f27a4ce2b33951e5f74e854c21a4
maleo <maleo@google.com>
2024-11-12 07:49:27 -0800

    SDK update
    GitOrigin-RevId: a670b534ec949735efd0f443c5829d15cc288907

--- intrinsic/util/runfiles.go
+++ intrinsic/util/runfiles.go
@@ -4,19 +4,41 @@
 package runfiles
 
 import (
+	"fmt"
+	"io/fs"
+	"path"
 
 	"github.com/bazelbuild/rules_go/go/runfiles"
 )
 
-// Root returns path to runfiles root
-func Root() (string, error) {
+// Rlocation gets the runfiles location of a file.
+//
+// Use the typical runfiles path without the repository name.
+//
+// Correct:
+//
+//	Rlocation("intrinsic/skills/build_defs/tests/no_op_skill_py_manifest.pbbin")
+//
+// Incorrect:
+//
+//	Rlocation("intrinsic/skills/build_defs/tests/no_op_skill_py_manifest.pbbin")
+func Rlocation(p string) (string, error) {
 	r, err := runfiles.New()
 	if err != nil {
 		return "", err
 	}
-	root, err := r.Rlocation(".")
+	return r.Rlocation(path.Join("ai_intrinsic_sdks", p))
+}
+
+// RootFs gets an fs.FS for runfiles relative to the repo root.
+func RootFs() (fs.FS, error) {
+	r, err := runfiles.New()
 	if err != nil {
-		return "", err
+		return nil, fmt.Errorf("unable to create runfiles object: %v", err)
 	}
-	return root, nil
+	baseDirFs, err := fs.Sub(r, "ai_intrinsic_sdks")
+	if err != nil {
+		return nil, fmt.Errorf("unable to get runfiles sub: %v", err)
+	}
+	return baseDirFs, nil
 }
