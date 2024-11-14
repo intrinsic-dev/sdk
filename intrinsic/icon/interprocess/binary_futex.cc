57a123d6acaca7bb14b0eac33707a7057efd5921
nilsb <nilsb@google.com>
2024-11-14 07:41:22 -0800

    SDK update
    GitOrigin-RevId: f8c8e744f6d018e28a76c68704d16ed8d9ff0439

--- intrinsic/icon/interprocess/binary_futex.cc
+++ intrinsic/icon/interprocess/binary_futex.cc
@@ -7,6 +7,7 @@
 
 #include <atomic>
 #include <cerrno>
+#include <climits>
 #include <cstdint>
 #include <cstring>
 #include <ctime>
@@ -39,23 +40,41 @@ inline int64_t futex(std::atomic<uint32_t> *uaddr, int futex_op, uint32_t val,
                  FUTEX_BITSET_MATCH_ANY);
 }
 
-// Iff the value is one, we return true and set the value to zero.
-bool TryWait(std::atomic<uint32_t> &val) {
-  uint32_t one = 1;
-  return val.compare_exchange_strong(one, 0);
+// Atomically reads from `val` and sets `val` to kReady *if* it was kPosted.
+//
+// Returns the value of `val` before the reset.
+uint32_t TryWait(std::atomic<uint32_t> &val) {
+  uint32_t expected = BinaryFutex::kPosted;
+  // If `val != expected`, then this writes the _actual_ value of `val` into
+  // `expected`.
+  (void)val.compare_exchange_strong(expected, BinaryFutex::kReady);
+  return expected;
 }
 
 RealtimeStatus Wait(std::atomic<uint32_t> &val, const timespec *ts,
                     bool private_futex) {
   const absl::Time start_time = absl::Now();
   while (true) {
-    if (TryWait(val)) {
-      return OkStatus();
+    {
+      uint32_t value = TryWait(val);
+      switch (value) {
+        case BinaryFutex::kReady:
+          break;
+        case BinaryFutex::kClosed:
+          return AbortedError("BinaryFutex is closed, aborting Wait()");
+        case BinaryFutex::kPosted:
+          return OkStatus();
+        default:
+          return InternalError(RealtimeStatus::StrCat(
+              "BinaryFutex took unexpected value: ", value));
+      }
     }
 
-    // The value is not yet what we expect, let's wait for it.
-    auto ret = futex(&val, FUTEX_WAIT_BITSET | FUTEX_CLOCK_REALTIME, 0,
-                     private_futex, ts);
+    // The value is not yet what we expect, but still kReady. Let's wait for it
+    // to change. FUTEX_WAIT_BITSET sleeps for as long as the value is still
+    // equal to kReady.
+    auto ret = futex(&val, FUTEX_WAIT_BITSET | FUTEX_CLOCK_REALTIME,
+                     BinaryFutex::kReady, private_futex, ts);
     if (ret == -1 && errno == ETIMEDOUT) {
       return DeadlineExceededError(RealtimeStatus::StrCat(
           "Timeout after ",
@@ -76,7 +95,8 @@ RealtimeStatus Wait(std::atomic<uint32_t> &val, const timespec *ts,
 }  // namespace
 
 BinaryFutex::BinaryFutex(bool posted, bool private_futex)
-    : val_(posted == true ? 1 : 0), private_futex_(private_futex) {}
+    : val_(posted == true ? BinaryFutex::kPosted : BinaryFutex::kReady),
+      private_futex_(private_futex) {}
 BinaryFutex::BinaryFutex(BinaryFutex &&other) : val_(other.val_.load()) {}
 BinaryFutex &BinaryFutex::operator=(BinaryFutex &&other) {
   if (this != &other) {
@@ -85,8 +105,10 @@ BinaryFutex &BinaryFutex::operator=(BinaryFutex &&other) {
   return *this;
 }
 
+BinaryFutex::~BinaryFutex() { Close(); }
+
 RealtimeStatus BinaryFutex::Post() {
-  uint32_t zero = 0;
+  uint32_t expected = BinaryFutex::kReady;
   // We need to make a copy of the private_futex_ member variable. Otherwise,
   // we might end up with a data race if the thread destructing the futex
   // reads `val_` between the `compare_exchange_strong` and the
@@ -95,7 +117,7 @@ RealtimeStatus BinaryFutex::Post() {
   // Take the address before, since the class instance could be destroyed before
   // `futex()` is called.
   std::atomic<uint32_t> *val_addr = &val_;
-  if (val_.compare_exchange_strong(zero, 1)) {
+  if (val_.compare_exchange_strong(expected, BinaryFutex::kPosted)) {
     // `futex` could fail with EFAULT, if `val_addr` is not a valid
     // user-space address anymore. This can happen if another thread destroyed
     // the BinaryFutex between the previous line and this one. Therefore, we
@@ -107,6 +129,11 @@ RealtimeStatus BinaryFutex::Post() {
     //
     // One indicating that we wake up at most 1 other client.
     futex(val_addr, FUTEX_WAKE, 1, private_futex);
+  } else if (expected == BinaryFutex::kClosed) {
+    // When compare_exchange_strong returns false, it writes the current value
+    // of the atomic into `expected`. We can check this to see if the
+    // BinaryFutex is closed.
+    return AbortedError("BinaryFutex is closed, aborting Post()");
   }
   return OkStatus();
 }
@@ -123,8 +150,39 @@ RealtimeStatus BinaryFutex::WaitFor(absl::Duration timeout) const {
   return WaitUntil(absl::Now() + timeout);
 }
 
-bool BinaryFutex::TryWait() const { return icon::TryWait(val_); }
+bool BinaryFutex::TryWait() const {
+  return icon::TryWait(val_) == BinaryFutex::kPosted;
+}
 
 uint32_t BinaryFutex::Value() const { return val_; }
+
+void BinaryFutex::Close() {
+  // We need to make a copy of the private_futex_ member variable. Otherwise,
+  // we might end up with a data race if the thread destructing the futex
+  // reads `val_` between the `compare_exchange_strong` and the
+  // `futex` call.
+  const bool private_futex = private_futex_;
+  // Take the address before, since the class instance could be destroyed before
+  // `futex()` is called.
+  std::atomic<uint32_t> *val_addr = &val_;
+  // Only signal waiters if the value changed (i.e. if it *wasn't already*
+  // BinaryFutex::kClosed).
+  if (val_.exchange(BinaryFutex::kClosed) != BinaryFutex::kClosed) {
+    // `futex` could fail with EFAULT, if `val_addr` is not a valid
+    // user-space address anymore. This can happen if another thread destroyed
+    // the BinaryFutex between the previous line and this one. Therefore, we
+    // ignore the return value here. Another error value should only be EINVAL,
+    // which should never happen here
+    // ("EINVAL: The kernel detected an inconsistency between the user-space
+    // state at uaddr and the kernel state—that is, it detected a waiter which
+    // waits in FUTEX_LOCK_PI or FUTEX_LOCK_PI2 on uaddr.").
+    //
+    // INT_MAX indicates that we wake up *all* waiters. We don't do this in
+    // `Post()`, because waking an unknown number of waiters could take unbound
+    // time and compromise realtime correctness, but since `Close()` happens on
+    // shutdown, we're not worried about that here.
+    futex(val_addr, FUTEX_WAKE, INT_MAX, private_futex);
+  }
+}
 
 }  // namespace intrinsic::icon
--- intrinsic/icon/interprocess/binary_futex.h
+++ intrinsic/icon/interprocess/binary_futex.h
@@ -66,6 +66,22 @@ namespace intrinsic::icon {
 //
 class BinaryFutex {
  public:
+  // These constants aren't enum values, because `static_cast`ing them to
+  // uint32_t everywhere would only make the implementation of BinaryFutex
+  // unnecessarily verbose and harder to read.
+
+  // This is the base state of the futex (unless it is constructed with
+  // `posted == true`). A successful call to Wait() will reset the futex to this
+  // value.
+  static constexpr uint32_t kReady = 0;
+  // Calling `Post()` on a `BinaryFutex` writes this value to the underlying
+  // variable.
+  static constexpr uint32_t kPosted = 1;
+  // Calling `Close()` on a `BinaryFutex` writes this value to the underlying
+  // variable. This state is final, i.e. a BinaryFutex that is `kClosed` will
+  // never change to another state afterwards.
+  static constexpr uint32_t kClosed = 2;
+
   // `posted` sets the initial value of the futex, i.e. if set to
   // true it equals calling `Post()`.
   // Set `private_futex` to true, if the futex is only used in one process, e.g.
@@ -76,40 +92,80 @@ class BinaryFutex {
   BinaryFutex &operator=(const BinaryFutex &other) = delete;
   BinaryFutex(BinaryFutex &&other);
   BinaryFutex &operator=(BinaryFutex &&other);
+  ~BinaryFutex();
 
   // Posts on the futex and increases its value to one.
   // If the current value is already one, the value will not further increase.
+  // If the value changed, this wakes up to one thread waiting on this
+  // BinaryFutex.
+  //
   // Returns an internal error if the futex could not be increased.
   // Real-time safe.
   // Thread-safe.
   RealtimeStatus Post();
 
-  // Waits until the futex becomes one or the deadline exceeds.
-  // When futex becomes one, immediately sets it to zero and returns ok.
-  // Returns an internal error if the futex could not be accessed.
+  // Waits until the futex becomes `kPosted`, `kClosed` or the deadline expires.
+  // As soon as the futex takes either of the two values above, or if the futex
+  // already *has* that value when this function starts, this function
+  // immediately sets the futex to `kReady` and returns OkStatus. Checks the
+  // futex before checking the deadline. So if the futex is already `kPosted` or
+  // `kClosed`, the deadline is not checked.
+  //
+  // Returns InternalError if the futex could not be accessed.
+  // Returns AbortedError if the futex is `kClosed`.
+  // Returns DeadlineExceededError if the futex does not become either `kPosted`
+  // or `kClosed` within the deadline.
+  // Returns OkStatus and resets the futex to `kReady` on success (if the futex
+  // already is or becomes `kPosted` before the deadline).
+  //
   // Real-time safe when `deadline` is close enough.
-  // Checks the futex before checking the deadline. So if the futex is already
-  // posted (i.e. has value one), the deadline is not checked. Thread-safe.
+  // Thread-safe.
   RealtimeStatus WaitUntil(absl::Time deadline) const;
 
-  // Waits until the futex becomes one or the timeout exceeds.
-  // When futex becomes one, immediately sets it to zero and returns ok.
-  // Returns an internal error if the futex could not be accessed.
-  // Real-time safe when `timeout` is short enough.
-  // Checks the futex before checking the timeout. So if the futex is already
-  // posted (i.e. has value one), the timeout is not checked.
+  // Waits until the futex becomes `kPosted`, `kClosed` or the timeout expires.
+  // As soon as the futex takes either of the two values above, or if the futex
+  // already *has* that value when this function starts, this function
+  // immediately sets the futex to `kReady` and returns OkStatus. Checks the
+  // futex before checking the timeout. So if the futex is already `kPosted` or
+  // `kClosed`, the timeout is not checked.
+  //
+  // Returns InternalError if the futex could not be accessed.
+  // Returns AbortedError if the futex is `kClosed`.
+  // Returns DeadlineExceededError if the futex does not become either `kPosted`
+  // or `kClosed` within the timeout.
+  // Returns OkStatus and resets the futex to `kReady` on success (if the futex
+  // already is or becomes `kPosted` before the timeout).
+  //
+  // Real-time safe when `timeout` is close enough.
   // Thread-safe.
   RealtimeStatus WaitFor(absl::Duration timeout) const;
 
-  // Returns true if the current value is one and, in this case, sets the value
-  // to zero.
+  // Checks (without blocking) whether the current value of the futex indicates
+  // that it has been `Post()`ed.
+  // If it does, this function (atomically!) resets the futex value.
+  //
+  // Returns true if the futex was `Post()`ed (and this function subsequently
+  // reset the value).
+  // Returns false if the futex wasn't `Post()`ed. This can happen for two
+  // reasons: Either the futex wasn't `Post()`ed _yet_, or it is closed (see
+  // `Close()` below) and never will be `Post()`ed.
   bool TryWait() const;
 
   // Returns the current value of the futex.
-  // This can either be zero or one. The returned value might be outdated by the
-  // time the caller uses the value.
+  // This can either be kReady, kPosted or kClosed. The returned value might be
+  // outdated by the time the caller uses the value.
+  //
   // Real-time safe.
   uint32_t Value() const;
+
+  // Marks this BinaryFutex as closed. Any subsequent calls to `Post()`,
+  // `TryWait()`, `WaitFor()` or `WaitUntil()` will *immediately* return
+  // AbortedError (or, in `TryWait()`'s case, `false`).
+  //
+  // Like `Post()`, this wakes up to one waiting thread, so if there is an
+  // ongoing call to `WaitUntil()` or `WaitFor()`, that call will end and return
+  // AbortedError.
+  void Close();
 
  private:
   // The atomic value is marked as mutable to create a const correct public
