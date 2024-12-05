e779154caa59a6bb830b21e2c1ec5d3cf3d19d57
alexanderfaxa <alexanderfaxa@google.com>
2024-12-05 04:26:59 -0800

    SDK update
    GitOrigin-RevId: 4727345fe05872f85691056ca2a4fa80901e7d8b

--- intrinsic/icon/release/portable/init_xfa_absl.cc
+++ intrinsic/icon/release/portable/init_xfa_absl.cc
@@ -2,11 +2,19 @@
 
 #include <cstring>
 
+#include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
 #include "absl/flags/usage.h"
 #include "absl/log/globals.h"
 #include "absl/log/initialize.h"
+#include "absl/log/log.h"
+#include "absl/time/clock.h"
+#include "absl/time/time.h"
 #include "intrinsic/icon/utils/log.h"
+
+ABSL_FLAG(bool, sleep, false,
+          "(optional) Do nothing and sleep indefinitely. This is used to "
+          "download the image and avoid crash looping.");
 
 void InitXfa(const char* usage, int argc, char* argv[]) {
   if (usage != nullptr && strlen(usage) > 0) {
@@ -16,4 +24,11 @@ void InitXfa(const char* usage, int argc, char* argv[]) {
   absl::InitializeLog();
   absl::SetStderrThreshold(absl::LogSeverityAtLeast::kInfo);
   intrinsic::RtLogInitForThisThread();
+
+  if (absl::GetFlag(FLAGS_sleep)) {
+    LOG(INFO) << "Started with --sleep=true. Sleeping indefinitely...";
+    absl::SleepFor(absl::InfiniteDuration());
+  }
+
+  LOG(INFO) << "********* Process Begin *********";
 }
